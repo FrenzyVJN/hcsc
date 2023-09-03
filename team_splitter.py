98ad04ed80370bc26a61523c6525020981148b34
Vijayan Sankar <vijayan23110015@snuchennai.edu.in>
2023-09-03 19:00:57 +0530

    added colors

--- team_splitter.py
+++ team_splitter.py
@@ -24,19 +24,27 @@ def shuffle_teams():
 
 # Create the main window
 window = tk.Tk()
-window.title("Team Shuffler")
+window.title("Futuristic Team Shuffler")
 
-# Create and pack widgets
-num_people_label = tk.Label(window, text="Enter the number of people:")
+# Customize the colors
+bg_color = "#333333"
+fg_color = "#FFFFFF"
+accent_color = "#FF6600"
+
+# Set the window background color
+window.configure(bg=bg_color)
+
+# Create and pack widgets with updated styles
+num_people_label = tk.Label(window, text="Enter the number of people:", bg=bg_color, fg=fg_color)
 num_people_label.pack(pady=10)
 
 num_people_entry = tk.Entry(window)
 num_people_entry.pack()
 
-add_names_button = tk.Button(window, text="Add Names", command=lambda: add_name_entries(int(num_people_entry.get())))
+add_names_button = tk.Button(window, text="Add Names", command=lambda: add_name_entries(int(num_people_entry.get())), bg=accent_color, fg=bg_color)
 add_names_button.pack(pady=10)
 
-name_entries_frame = tk.Frame(window)
+name_entries_frame = tk.Frame(window, bg=bg_color)
 name_entries_frame.pack()
 
 def add_name_entries(num_entries):
@@ -44,7 +52,7 @@ def add_name_entries(num_entries):
     name_entries = []  # Store the Entry widgets for later retrieval
 
     for i in range(num_entries):
-        label = tk.Label(name_entries_frame, text=f"Name {i + 1}:")
+        label = tk.Label(name_entries_frame, text=f"Name {i + 1}:", bg=bg_color, fg=fg_color)
         label.grid(row=i, column=0, padx=10, pady=5)
 
         entry = tk.Entry(name_entries_frame)
@@ -52,18 +60,18 @@ def add_name_entries(num_entries):
 
         name_entries.append(entry)
 
-    shuffle_button.config(state=tk.NORMAL)  # Enable the shuffle button
+    shuffle_button.config(state=tk.NORMAL, bg=accent_color, fg=bg_color)  # Enable the shuffle button
 
-shuffle_button = tk.Button(window, text="Shuffle Teams", command=shuffle_teams, state=tk.DISABLED)
+shuffle_button = tk.Button(window, text="Shuffle Teams", command=shuffle_teams, state=tk.DISABLED, bg=accent_color, fg=bg_color)
 shuffle_button.pack(pady=10)
 
-team1_label = tk.Label(window, text="Team 1:")
+team1_label = tk.Label(window, text="Team 1:", bg=bg_color, fg=fg_color)
 team1_label.pack(side=tk.LEFT, padx=10)
 
-team2_label = tk.Label(window, text="Team 2:")
+team2_label = tk.Label(window, text="Team 2:", bg=bg_color, fg=fg_color)
 team2_label.pack(side=tk.RIGHT, padx=10)
 
-result_label = tk.Label(window, text="")
+result_label = tk.Label(window, text="", bg=bg_color, fg=accent_color)
 result_label.pack(pady=10)
 
 # Start the GUI main loop
